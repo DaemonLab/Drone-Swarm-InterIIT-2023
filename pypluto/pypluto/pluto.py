1e79eac5ae4c7b281f1d0a7c9f1fc35496873873
AkshitRaizada <105885452+AkshitRaizada@users.noreply.github.com>
2022-12-25 20:46:19 +0530

    Update pluto.py

--- pypluto/pypluto/pluto.py
+++ pypluto/pypluto/pluto.py
@@ -1,5 +1,6 @@
 from pypluto.Comm.server import Connection
 from pypluto.Comm.msg import Message
+from pypluto.commands.movement import Moving
 import numpy as np
 
 
@@ -7,80 +8,18 @@ class Drone():
     def __init__(self, DroneIP="192.168.4.1", DronePort="23"):
         self.DRONEIP = DroneIP
         self.DRONEPORT = DronePort
-        self.conn = Connection(self.DRONEIP, self.DRONEPORT).connect()
+        self.conn = Connection(self.DRONEIP, self.DRONEPORT().connect()
         self.msg = Message()
-
-    def takeOff(self):
-        self.sendData(self.msg.set_command(1), "takeOff")
-
-    def land(self):
-        self.sendData(self.msg.set_command(2), "Land")
-
-    def backFlip(self):
-        self.sendData(self.msg.set_command(3), "BackFlip")
-
-    def frontFlip(self):
-        self.sendData(self.msg.set_command(4), "frontFlip")
-
-    def rightFlip(self):
-        self.sendData(self.msg.set_command(5), "rightFlip")
-
-    def leftFlip(self):
-        self.sendData(self.msg.set_command(6), "LeftFlip")
+        self.move_cmd = Moving()
     
-    def arming(self, arm: bool):
-        L = 1000  # LOW
-        C = 1500  # center
-        H = 2000  # High
-        RC_ROLL, RC_PITCH, RC_YAW, RC_THROTTLE, RC_AUX1, RC_AUX2, RC_AUX3, RC_AUX4 = C, C, L, L, 1500, 1000, 1500, 1200
-        data = [RC_ROLL, RC_PITCH, RC_YAW, RC_THROTTLE, RC_AUX1, RC_AUX2, RC_AUX3, RC_AUX4]
-        if arm:
-            data[-1] = 1500
-            self.sendData(self.msg.set_raw_rc(data),"ARM")
-        else:
-            data[-1] = 901
-            self.sendData(self.msg.set_raw_rc(data),"DISARM")
-    
+    def arm():
+        self.move_cmd.arming(True)   
         
-    def move(self, direction, *args):
-        center = np.array([1500, 1500, 1500, 1500]) #RC_ROLL, RC_PITCH, RC_YAW, RC_THROTTLE
-
-        RC_AUX1, RC_AUX2, RC_AUX3, RC_AUX4 = 1500, 1500, 1500, 1500
-        speed = 100
-        for arg in args:
-            speed = int(arg)
-        
-        if speed>2100:
-            print("Clipping speed to 2100")
-            speed = 2100
-        if speed<900:
-            print("Clipping speed to 900")
-            speed = 900
-
-        change = {
-            "forward": np.array([0, speed, 0, 0]),
-            "backward": np.array([0, -speed, 0, 0]),      
-            "left": np.array([-speed, 0, 0, 0]),      
-            "right": np.array([speed, 0, 0, 0]),
-            "up": np.array([0, 0, 0, speed]),
-            "down": np.array([0, 0, 0, -speed]),
-            "clck": np.array([0, 0, speed, 0]),
-            "anticlck": np.array([0, 0, -speed, 0]),
-            "Y": np.array([0, speed, 0, 0]),
-            "X": np.array([speed, 0, 0, 0]),
-            "Z": np.array([0, 0, 0, speed])
-        }
-        
-        RC_ROLL, RC_PITCH, RC_YAW, RC_THROTTLE,  = center + change[direction]
-        data = [RC_ROLL, RC_PITCH, RC_YAW, RC_THROTTLE, RC_AUX1, RC_AUX2, RC_AUX3, RC_AUX4]
-        self.sendData(self.msg.set_raw_rc(data),"Move")
-
-    def arm(self):
-        self.arming(True)
-
     def disarm(self):
-        self.arming(False)
+        self.move_cmd.arming(False)
 
+    def move(self, direction, *args):
+        self.move_cmd.move(direction, *args)
 
     def sendData(self, data, err):
         try:
