26b19782ab5d87d02c35f79157e7d92bf867b888
KshitijBhat <me200003042@iiti.ac.in>
2022-12-26 13:04:05 +0530

    documentation

--- pypluto/pypluto/commands/movement.py
+++ pypluto/pypluto/commands/movement.py
@@ -2,51 +2,80 @@ from pypluto.Comm.server import Connection
 from pypluto.Comm.msg import Message
 import numpy as np
 
-class Moving():
+class Move():
 
     def __init__(self):
         self.msg = Message()
 
     def arming(self, arm: bool):
+        """
+        Parses the arm and disarm commands.
+
+        Parameters
+        ----------
+        arm : bool
+            True for arm and False for disarm.
+        Returns
+        -------
+        parsed : bytes
+            The parsed data to be sent to the drone.
+        """
+
         RC_ROLL, RC_PITCH, RC_THROTTLE, RC_YAW, RC_AUX1, RC_AUX2, RC_AUX3, RC_AUX4 = 1500, 1500, 1000, 1700, 1500, 1000, 1500, 1500
         data = [RC_ROLL, RC_PITCH, RC_THROTTLE, RC_YAW, RC_AUX1, RC_AUX2, RC_AUX3, RC_AUX4]
         if arm:
             data[-1] = 1500
-            return (self.msg.set_raw_rc(data))
         else:
             data[-1] = 901
-            return (self.msg.set_raw_rc(data))
+
+        parsed = self.msg.set_raw_rc(data)
+        return parsed
 
     
-    def move(self, direction, *args):
+    def steer_cmd(self, direction:str, magnitude:int=100):
+        """
+        Parses the steer commands.
+
+        Parameters
+        ----------
+        direction : str
+            Valid inputs - "forward", "backward", "left", "right", "up", "down", "pitch", "roll", "throttle" and "yaw".
+        magnitude : int
+            Magnitude over which the drone steers, -600<magnitude<600
+
+        Returns
+        -------
+        parsed : bytes
+            The parsed data to be sent to the drone.
+        """
+
         center = np.array([1500, 1500, 1500, 1500])
 
         RC_AUX1, RC_AUX2, RC_AUX3, RC_AUX4 = 1500, 1500, 1500, 1500
-        speed = 100
-        for arg in args:
-            speed = int(arg)
     
-        if speed + 1500 > 2100:
-            print("Clipping speed to 2100")
-            speed = 2100
-        if speed + 1500 < 900:
-            print("Clipping speed to 900")
-            speed = 900
+        if magnitude + 1500 > 2100:
+            print("Clipping magnitude to 2100")
+            magnitude = 2100
+        if magnitude + 1500 < 900:
+            print("Clipping magnitude to 900")
+            magnitude = 900
 
         change = {
-            "forward": np.array([0, speed, 0, 0]),
-            "backward": np.array([0, -speed, 0, 0]),      
-            "left": np.array([-speed, 0, 0, 0]),      
-            "right": np.array([speed, 0, 0, 0]),
-            "up": np.array([0, 0, 0, speed]),
-            "down": np.array([0, 0, 0, -speed]),
-            "clck": np.array([0, 0, speed, 0]),
-            "anticlck": np.array([0, 0, -speed, 0]),
-            "Y": np.array([0, speed, 0, 0]),
-            "X": np.array([speed, 0, 0, 0]),
-            "Z": np.array([0, 0, 0, speed])
+            "forward": np.array([0, magnitude, 0, 0]),
+            "backward": np.array([0, -magnitude, 0, 0]),      
+            "left": np.array([-magnitude, 0, 0, 0]),      
+            "right": np.array([magnitude, 0, 0, 0]),
+            "up": np.array([0, 0, magnitude, 0]),
+            "down": np.array([0, 0, -magnitude, 0]),
+            "clck": np.array([0, 0, 0, magnitude]),
+            "anticlck": np.array([0, 0, 0, -magnitude]),
+            "roll": np.array([magnitude, 0, 0, 0]),
+            "pitch": np.array([0, magnitude, 0, 0]),
+            "throttle": np.array([0, 0, magnitude, 0]),
+            "yaw": np.array([0, 0, 0, magnitude])
         }
     
         RC_ROLL, RC_PITCH, RC_THROTTLE, RC_YAW,  = center + change[direction]
         data = [RC_ROLL, RC_PITCH, RC_THROTTLE, RC_YAW, RC_AUX1, RC_AUX2, RC_AUX3, RC_AUX4]
-        return (self.msg.set_raw_rc(data))
+        parsed = self.msg.set_raw_rc(data)
+        return parsed
--- pypluto/pypluto/pluto.py
+++ pypluto/pypluto/pluto.py
@@ -1,6 +1,6 @@
 from pypluto.Comm.server import Connection
 from pypluto.Comm.msg import Message
-from pypluto.commands.movement import Moving
+from pypluto.commands.movement import Move
 import numpy as np
 
 
@@ -10,7 +10,7 @@ class Drone():
         self.DRONEIP = DroneIP
         self.DRONEPORT = DronePort
         self.conn = Connection(self.DRONEIP, self.DRONEPORT).connect()
-        self.move_cmd = Moving()
+        self.move_cmd = Move()
 
     def arm(self):
         self.sendData(self.move_cmd.arming(True), "ARM")   
@@ -18,10 +18,20 @@ class Drone():
     def disarm(self):
         self.sendData(self.move_cmd.arming(False), "DISARM")
 
-    def move(self, direction, *args):
-        self.sendData(self.move_cmd.move(direction, *args), "MOVE")
+    def steer(self, direction:str, magnitude:int=100):
+        """
+        Steers the drone in the specified direction or angle.
 
-    def sendData(self, data, err):
+        Parameters
+        ----------
+        direction : str
+            Valid inputs - "forward", "backward", "left", "right", "up", "down", "pitch", "roll", "throttle" and "yaw".
+        magnitude : int
+            Magnitude over which the drone steers, -600<magnitude<600
+        """
+        self.sendData(self.move_cmd.steer_cmd(direction, magnitude), f"STEER {direction}")
+
+    def sendData(self, data:bytes, err:str):
         try:
             print(data)
             self.conn.write(data)
