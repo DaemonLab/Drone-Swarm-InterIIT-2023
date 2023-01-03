cbb006b72308377c11431598d390a39936964064
Sairaj Loke <104747561+SairajLoke@users.noreply.github.com>
2023-01-03 12:25:02 +0530

    Add files via upload

---
+++ PyDrona/PlutoOPT/server/connection.py
@@ -0,0 +1,24 @@
+import telnetlib
+
+# DRONEIP = "192.168.4.1"
+# DRONEPORT = "23"
+
+class Connection():
+
+    def __init__(self, DroneIP="192.168.4.1", DronePort="23"):
+        self.DRONEIP = DroneIP
+        self.DRONEPORT = DronePort
+        
+    def connect():
+        try:
+            tn=telnetlib.Telnet(DRONEIP,DRONEPORT)
+            print("\nPluto connected")
+        except:
+            print("\nCannot connect to pluto, please try again...")
+
+    def disconnect(self):
+        self.tn.close()
+        print("\nPluto disconnected")
+
+    def create_data_packet( self, cmd_list ):
+        self.
