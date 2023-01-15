fbffc4a277a1508350d9feaf914edc2d43c5fe55
Yeeshukant <yeeshukantsingh@gmail.com>
2023-01-15 18:06:50 +0530

    all function in one file in pluto dir

--- pypluto/main.py
+++ pypluto/main.py
@@ -7,25 +7,25 @@ if __name__ == '__main__':
     client2 = Drone("192.168.4.1","23")
     #client1.disarm()
     client2.disarm()
-    time.sleep(5)
+    #time.sleep(5)
     #client1.arm()
     # time.sleep(5)
     client2.arm()
     time.sleep(5)
-    client2.trim(5, -25, 0, 0)
+    #client2.trim(5, -25, 0, 0)
     #client1.steer("up",400)
-    # client2.steer("up",450)
-    start = time.time()
-    now = time.time()
-    gap = now -start
+    client2.steer("up",600)
+    #start = time.time()
+    #now = time.time()
+    #gap = now -start
     # while (gap < 5):
     #     print("a")
     #     now = time.time()
     #     gap = now - start 
     #     client2.takeoff()
-    client2.takeoff()
+    #client2.takeoff()
     # client2.backFlip()
-    time.sleep(5)
+    time.sleep(2)
 
     client2.set_steer([0, 0, 220, 0])
     time.sleep(1)
